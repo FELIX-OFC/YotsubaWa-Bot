e4d3feb082d3f0bf94dfbddd0045c4878af91d82
PixelCrew Team <pixelcrewteam@gmail.com>
2025-11-22 19:29:36 -0400

    Actualizar grupo-demote.js

--- plugins/grupo-demote.js
+++ plugins/grupo-demote.js
@@ -3,21 +3,21 @@ const handler = async (m, { conn }) => {
     // let who = m?.message?.extendedTextMessage?.contextInfo?.participant || m?.mentionedJid[0] || await m?.quoted?.sender;
    let texto = await m.mentionedJid
    let who = texto.length > 0 ? texto[0] : (m.quoted ? await m.quoted.sender : false)
-    if (!who) return m.reply('*ᐛ👑* Mensiona a un ciudadano de este mundo mágico para quitarle sus *privilegios altos.*');
+    if (!who) return m.reply('*ᐛ❄* Mensiona a un ciudadano de este mundo mágico para quitarle sus *privilegios altos.*');
 
     const groupMetadata = await conn.groupMetadata(m.chat);
     const participant = groupMetadata.participants.find(participant => participant.jid === who);
 
     if (!participant || !participant.admin) {
-    return conn.reply(m.chat, `🤨 *@${who.split('@')[0]}* no es administrador del grupo!`, m, { mentions: [who] });
+    return conn.reply(m.chat, `☃️ *@${who.split('@')[0]}* no es administrador del grupo!`, m, { mentions: [who] });
     }
 
     if (who === groupMetadata.owner) {
-      return m.reply('🤨 ¿Quieres que te quite admin?');
+      return m.reply('🎁 ¿Quieres que te quite admin?');
     }
 
     if (who === conn.user.jid) {
-      return m.reply('😒 No puedo quitarme admin yo misma we');
+      return m.reply('🎄 No puedo quitarme admin yo misma we');
     }
 
     await conn.groupParticipantsUpdate(m.chat, [who], 'demote');
