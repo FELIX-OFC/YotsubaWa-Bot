ced55a83ae5d14f8e082286e035e1f0e9f5ad9ae
FELIX-OFC <felixofc3@gmail.com>
2025-12-27 13:03:23 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1,3 +1,8 @@
+/*
+Código de yukiBot-MD
+Dejen los créditos de ese hermoso bot
+https://github.com/YukiBot-MD
+*/
 import { smsg } from "./lib/simple.js"
 import { format } from "util"
 import { fileURLToPath } from "url"
